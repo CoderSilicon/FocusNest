926f14044ba851a22ab4358fa86588b101970571
CoderSilicon <149225177+CoderSilicon@users.noreply.github.com>
2025-07-26 21:46:01 +0530

    First Instance

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Welcome to Vue!</h1>
+    <p>This is a basic Vue template.</p>
+  </div>
+</template>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
+
+<script lang="ts">
+export default {
+  name: 'App',
+}
+</script>
